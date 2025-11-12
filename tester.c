961881c01de278c08db5b3dd98e96ed0f2b1e3ed
Trevor M <trevordmooney@gmail.com>
2025-11-11 17:57:07 -0800

    feat: 11 asm functions ready for testing

--- tester.c
+++ tester.c
@@ -7,9 +7,24 @@ ssize_t my_read(int fd, const char *buf, size_t count);
 ssize_t my_write(int fd, const char *buf, size_t count);
 size_t my_strlen(const char *s);
 char *my_strchr(const char *s, int c);
+char *my_index(const char *s, int c);
 int my_strcmp(const char *s1, const char *s2);
 int my_strncmp(const char *s1, const char *s2, size_t num);
 int my_strcasecmp(const char *s1, const char *s2);
+
+
+void *my_memcpy(void *dest_str, const void * src_str, size_t n);
+void * my_memset ( void * ptr, int value, size_t num );
+void* my_memmove( void* dest, const void* src, size_t count);
+
+/*
+       DESCRIPTION
+       The memmove() function copies n bytes from memory area src to
+       memory area dest.  The memory areas may overlap: copying takes
+       place as though the bytes in src are first copied into a temporary
+       array that does not overlap src or dest, and the bytes are then
+       copied from the temporary array to dest.
+*/
 
 int main(void){
 	//my_read
@@ -67,6 +82,45 @@ int main(void){
     //my_strcasecmp
     printf("Apple vs. apple test: %d\n", my_strcasecmp("Apple", "apple")); // 0
 
+    //my_index
+	const char msg1[] = "Hello from TV land!";
+    printf("before: %s\n", msg1);
+    char letter1 = 'f';
+    char *aft1 = my_index(msg1, letter1);
+    printf("after: %s\n", aft1);
 
+    printf("////////////////////////////\n");
+    //my_memset
+    printf("my_memset: \n");
+    char *testing = malloc(100 * sizeof(char));
+    strcpy(testing, "Hello from TV land!");
+	char msg2[] = "Hello from TV land!";
+    char *res2 = my_memset(msg2, 'x', 7);
+    printf("after: %s\n", res2);
+    printf("after: %s\n", msg2);
+
+    printf("before: %s\n", testing);
+    char *res3 = my_memset(testing, 'x', 7);
+    printf("after: %s\n", res3);
+    printf("after: %s\n", testing);
+
+    printf("////////////////////////////\n");
+    //my_memcpy
+    printf("my_memcopy:\n");
+    char *dst1 = malloc(100 * sizeof(char));
+    strcpy(dst1, "Hello from TV land!");
+    char *src1 = malloc(100 * sizeof(char));
+    strcpy(src1, "WWWW");
+    printf("before dst1: %s\n", dst1);
+    printf("before src1: %s\n", src1);
+    char *res4 = my_memcpy(dst1, src1, 3);
+    printf("after res: %s\n", res4);
+    printf("after dst1: %s\n", dst1);
+
+    printf("////////////////////////////\n");
+    //my_memmove
+    char bufz[10] = "abcdef";
+    my_memmove(bufz + 2, bufz, 5);
+    printf("%s\n", bufz);
     return 0;
 }
